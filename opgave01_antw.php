2836b64ab1aa6ac1607efd2419de9fc38174bd28
bluhb <35539085+bluhb@users.noreply.github.com>
2018-01-17 22:39:04 +0100

    Add files via upload

---
+++ opgave01_antw.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[Vul hier je naam en studentnummer in]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Opgave 1</title>
+    </head>
+    <body>
+        <h1>Opgave 1</h1>
+        <?php
+        /* Gebruik onderstaande variabele in de uitwerking */
+        $woorden = "hallo hoi";
+
+        /* Begin uitwerking */
+
+        print(ucwords($woorden));
+
+        /* Einde uitwerking */
+        ?>
+    </body>
+</html>
