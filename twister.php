0b4d03b0b2b52b3d6581c0650f8e674c01123c56
bluhb <35539085+bluhb@users.noreply.github.com>
2018-01-17 23:56:13 +0100

    troubleshoot

--- twister.php
+++ twister.php
@@ -11,7 +11,12 @@
 		<?php
 		if(isset($_POST["Aantal"])){
 			$spelers = $_POST["Aantal"];
-			print("aantal spelers". $spelers);
+			print("aantal spelers ". $spelers);
+			print('<form action="game.php" method="POST">')
+			for($i=1, $i <= $spelers, $i++){
+				print('Kleuren(gescheiden door komma)<input type="text" name="kleuren">');
+			}
+			print('</form>')
 		}
 		else{
 			print("you failed");
