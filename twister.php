b9ecf8ba5a8fe5c0bf1238dbc765ae329ed1726e
Bart <bart.oevering@student.hu.nl>
2018-01-18 15:38:50 +0100

    Add max value to number fiels.

--- twister.php
+++ twister.php
@@ -5,8 +5,8 @@
 	</head>
 	<body>
 		<form action="twister.php" method="POST">
-			Aantal Spelers? <input type="number" name="AmountPlayers"> <br>
-			Aantal Kleuren per speler? <input type="number" name="AmountColors"> <br>
+			Aantal Spelers? <input type="number" max="10" name="AmountPlayers"> <br>
+			Aantal Kleuren per speler? <input type="number" max="10" name="AmountColors"> <br>
 			<input type="submit" value="Doorgaan"><input type="reset" value="Reset">
 		</form>
 		<?php
