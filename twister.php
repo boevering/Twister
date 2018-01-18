c4f3719c8fd654d5075d895cd722bda1417f6dcb
Bart <bart.oevering@student.hu.nl>
2018-01-18 15:15:58 +0100

    Add amount of colors for players

--- twister.php
+++ twister.php
@@ -5,16 +5,17 @@
 	</head>
 	<body>
 		<form action="twister.php" method="POST">
-			Aantal Spelers<input type="number" name="Aantal">
+			Aantal Spelers?<input type="number" name="Aantal">
+			Aantal Kleuren per speler?<input type="number" name="AantalKleuren">
 			<input type="submit">
 		</form>
 		<?php
-		if(isset($_POST["Aantal"])){
+		if(isset($_POST["Aantal"] && $_POST["AantalKleuren"])){
 			$spelers = $_POST["Aantal"];
-			print("<br>Aantal spelers ". $spelers."<br>");
+			print("<br>Aantal spelers: ". $spelers. "<br>");
 			print('<form action="game.php" method="POST">');
 			for($i=1; $i <= $spelers; $i++){
-				print('Kleuren(gescheiden door komma)<input type="text" name="kleuren"><br>');
+				print('Kleuren (gescheiden door komma)<input type="text" name="kleuren"><br>');
 			}
 			print('<input type="submit"');
 			print('</form>');
