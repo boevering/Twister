c23e13ce2afc5cfca1281cc8cf936132675ebc38
Bart <bart.oevering@student.hu.nl>
2018-01-18 15:25:43 +0100

    Test of change to players and colors

--- twister.php
+++ twister.php
@@ -5,17 +5,20 @@
 	</head>
 	<body>
 		<form action="twister.php" method="POST">
-			Aantal Spelers?<input type="number" name="Aantal"> <br>
-			Aantal Kleuren per speler?<input type="number" name="AantalKleuren"> <br>
+			Aantal Spelers? <input type="number" name="AmountPlayers"> <br>
+			Aantal Kleuren per speler? <input type="number" name="AmountColors"> <br>
 			<input type="submit">
 		</form>
 		<?php
-		if(isset($_POST["Aantal"])){
-			$spelers = $_POST["Aantal"];
-			print("<br>Aantal spelers: ". $spelers. "<br>");
+		if(isset($_POST["AmountPlayers"], $_POST["AmountColors"])){
+			$amountplayers = $_POST["AmountPlayers"];
+			$amountcolors = $_POST["AmountColors"];
+			print("<br>Aantal gekozen spelers: ". $amountplayers. "<br>");
 			print('<form action="game.php" method="POST">');
-			for($i=1; $i <= $spelers; $i++){
-				print('Kleuren (gescheiden door komma)<input type="text" name="kleuren"><br>');
+			for($i=1; $i <= $amountplayers; $i++){
+				for($o=1; $o <= $amountcolors; $o++){
+					print('Speler: '.$i.', Kleur '.$o.': <input type="text" name="player'.$i.'color'.$o.'"><br>');
+				}
 			}
 			print('<input type="submit"');
 			print('</form>');
