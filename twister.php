e086156812e207b4e07fff197946992bb8134725
Bart <bart.oevering@student.hu.nl>
2018-01-18 15:53:36 +0100

    And we need php for that

--- twister.php
+++ twister.php
@@ -5,8 +5,8 @@
 	</head>
 	<body>
 		<form action="twister.php" method="POST">
-			Aantal Spelers? <input type="number" max="10" name="AmountPlayers" value=$_POST["AmountPlayers"]> <br>
-			Aantal Kleuren per speler? <input type="number" max="10" name="AmountColors" value=$_POST["AmountColors"]> <br>
+			Aantal Spelers? <input type="number" max="10" name="AmountPlayers" value=<?php $_POST["AmountPlayers"]?>> <br>
+			Aantal Kleuren per speler? <input type="number" max="10" name="AmountColors" value=<?php $_POST["AmountColors"]?>> <br>
 			<input type="submit" value="Doorgaan"><input type="reset" value="Reset">
 		</form>
 		<?php
