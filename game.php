3a13a2ec17e8907987eb7303e332c3c9589452ba
bluhb <35539085+bluhb@users.noreply.github.com>
2018-01-18 22:05:46 +0100

    a

--- game.php
+++ game.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<?php
-			$klaas = hash('ripemd160', 'The quick brown fox jumped over the lazy dog.'); 
+			$klaas = hash('sha265', 'The quick brown fox jumped over the lazy dog.'); 
 			print($klaas);
 			
 			
