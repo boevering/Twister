a3d5caa0ddfcd6a490031c089ccc72a4acc84b2c
bluhb <35539085+bluhb@users.noreply.github.com>
2018-01-18 23:01:28 +0100

    a

--- game.php
+++ game.php
@@ -16,7 +16,7 @@ require_once("functions.php");
 		$queryusers = 'SELECT * FROM `game` WHERE `gameid` = "'.$gameid.'";';
 		$db = mysqli_query($dbtwister, $queryusers);
 		print("klaas");
-		print($db);
+		print($queryusers);
 		while ( $d=mysqli_fetch_assoc($db)) {
   					print_r($d);
 				}
