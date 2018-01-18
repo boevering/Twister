b76285f64c157bec705cd126ab7491301980369d
bluhb <35539085+bluhb@users.noreply.github.com>
2018-01-18 22:03:45 +0100

    a

--- game.php
+++ game.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<?php
-			print(hash('ripemd160', 'The quick brown fox jumped over the lazy dog.')); 
+			echo hash('ripemd160', 'The quick brown fox jumped over the lazy dog.'); 
 			
 			
 			
