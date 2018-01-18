071963201a695e173fc7dce10cebbe70174f1f67
Bart <bart.oevering@student.hu.nl>
2018-01-18 16:19:13 +0100

    Create seperate file for functions

--- twister.php
+++ twister.php
@@ -1,5 +1,8 @@
 <?php 
+# Let's make sure the database is connected, use $dbtwister as database (file on server, NOT on Github!)
 require_once("Connections/twister.php");
+# Lets use a seperate file for functions
+require_once("functions.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,19 +32,8 @@ require_once("Connections/twister.php");
 			print('<input type="submit" value="Doorgaan"><input type="reset" value="Reset">');
 			print('</form>');
 		}
+		$TestDatabase()
 		?>
-		<?php
-		if (!$dbtwister) {
-			echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			exit;
-		}
 
-		echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-		echo "Host information: " . mysqli_get_host_info($dbtwister) . PHP_EOL;
-
-		mysqli_close($dbtwister);
-		?>
 	</body>
 </html>
