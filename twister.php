7575cc9de54df07d423b4bf66eb1ba1f5a57e23b
Bart <bart.oevering@student.hu.nl>
2018-01-22 13:17:43 +0100

    remove debug

--- twister.php
+++ twister.php
@@ -25,9 +25,7 @@ $_SESSION["debug"] = "0";
 		if(isset($_POST["insert"])){
 			$InsertPlayer = "INSERT INTO `users`(`firstname`, `prefix`, `lastname`) VALUES ('".test_input($_POST["firstname"])."','".test_input($_POST["prefix"])."','".test_input($_POST["lastname"])."');";
 			$dbGM = mysqli_query($dbtwister, $InsertPlayer);
-			
-			print($InsertPlayer);
-			
+						
 			$_POST["AmountPlayers"] = $_SESSION["amountplayers"];
 			$_POST["AmountColors"] = $_SESSION["amountcolors"];
 		}
