ebf7e6513031ba4c018819bdc5e97572473ccda3
Bart <bart.oevering@student.hu.nl>
2018-01-22 11:22:51 +0100

    Change to: style.display=

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,8 @@
 function display() {
             var z = prompt("enter name...");
             if(z !== null) {
-                document.getElementById("firstname").style.visibility = 'block';
-				document.getElementById("prefix").style.visibility = 'block';
-				document.getElementById("lastname").style.visibility = 'block';
+                document.getElementById("firstname").style.display='block';
+				document.getElementById("prefix").style.display='block';
+				document.getElementById("lastname").style.display='block';
             }
         }
