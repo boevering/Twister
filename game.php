a5459c24bcb004ef487cdfc4c453fa26615c302f
Bart <bart.oevering@student.hu.nl>
2018-01-22 14:04:40 +0100

    .

--- game.php
+++ game.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION["debug"])){$_SESSION["debug"] = "0";};
 		<title>Twister</title>
 		<script type="text/javascript">
 
-			COUNTER_START = <?php echo $_POST["refeshtimer"]; ?>
+			COUNTER_START = 30
 
 			function tick () {
 				if (document.getElementById ('counter').firstChild.data > 0) {
