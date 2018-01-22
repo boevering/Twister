ed84e719b258a469f89e6b404444f666e7a2012d
Bart <bart.oevering@student.hu.nl>
2018-01-22 11:23:49 +0100

    Remove some code

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,6 @@
 // JavaScript Document
 function display() {
-            var z = prompt("enter name...");
-            if(z !== null) {
-                document.getElementById("firstname").style.display='block';
-				document.getElementById("prefix").style.display='block';
-				document.getElementById("lastname").style.display='block';
-            }
-        }
+	document.getElementById("firstname").style.display='block';
+	document.getElementById("prefix").style.display='block';
+	document.getElementById("lastname").style.display='block';
+}
